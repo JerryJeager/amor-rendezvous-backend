8ada2f15d9c029f73069943ba4db274d6a059650
Amadi Jerry <amadijerry310@gmail.com>
2024-04-05 09:11:04 +0100

    add auth

--- utils/token.go
+++ utils/token.go
@@ -2,12 +2,55 @@ package utils
 
 import (
 	// "os"
+	"errors"
+	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/JerryJeager/amor-rendezvous-backend/service"
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 )
+
+func ValidateToken (c *gin.Context) error {
+    token, err := GetToken(c)
+
+    if err != nil {
+        return err
+    }
+
+    _, ok := token.Claims.(jwt.MapClaims)
+    if ok && token.Valid {
+        return nil
+    }
+
+    return errors.New("invalid token provided")
+}
+
+func GetToken(c *gin.Context) (*jwt.Token, error) {
+    tokenString := getTokenFromRequest(c)
+    token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+        if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+        }
+
+        return "", nil
+    })
+    return token, err
+}
+
+func getTokenFromRequest(c *gin.Context) string {
+    bearerToken := c.Request.Header.Get("Authorization")
+
+    splitToken := strings.Split(bearerToken, " ")
+    if len(splitToken) == 2 {
+        return splitToken[1]
+    }
+    return ""
+}
+
+
 
 func GenerateToken(user service.User) (string, error) {
 
