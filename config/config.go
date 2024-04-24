9273fedc796543317145bb07c392d091dba38a48
Amadi Jerry <amadijerry310@gmail.com>
2024-04-24 06:53:26 +0100

    cleanup config

--- config/config.go
+++ config/config.go
@@ -20,21 +20,21 @@ func GetSession() *gorm.DB{
 
 func ConnectToDB(){
 
+	//local DB config:::
 	// host := os.Getenv("HOST")
 	// username := os.Getenv("USER")
 	// password := os.Getenv("PASSWORD")
 	// port := os.Getenv("DBPORT")
 	// dbName := os.Getenv("DBNAME")
 
-	connectionString := os.Getenv("CONNECTION_STRING")
-
 	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, username, password, dbName, port)
 
-	// dsn := "host=localhost user=postgres password=chidiebere823A dbname=amor_rendezvous port=5432 sslmode=disable"
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
+
+	//live DB config:::
+	connectionString := os.Getenv("CONNECTION_STRING")
     db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
-	
-    // db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil{
 		log.Fatal(err)
